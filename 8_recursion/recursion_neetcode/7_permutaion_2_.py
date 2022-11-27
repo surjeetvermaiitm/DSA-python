cb260794f8f1d45719ce43c62fad1abf05679d60
surjeetvermaiitm <surjeetverma2017@gmail.com>
2022-11-27 15:00:49 +0530

    sorting

--- 8_recursion/recursion_neetcode/7_permutaion_2_.py
+++ 8_recursion/recursion_neetcode/7_permutaion_2_.py
@@ -1,2 +1,23 @@
 #Link: https://leetcode.com/problems/permutations-ii/
 
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        perm=[]
+        
+        count={n:0 for n in nums}
+        for n in nums:
+            count[n]+=1
+        def dfs():
+            if len(perm)==len(nums):
+                res.append(perm[:])
+                return
+            for n in count:
+                if count[n]>0:
+                    perm.append(n)
+                    count[n]-=1
+                    dfs()
+                    perm.pop()
+                    count[n]+=1
+        dfs()
+        return res
---
+++ 8_recursion/recursion_neetcode/8_word_search.py
@@ -0,0 +1,2 @@
+#Link: https://leetcode.com/problems/word-search/
+
